69d177c972c8f622e26b83f71173b7130ba15c47
Gabriel <gabrieldmonteiro@live.com>
2020-05-11 21:41:45 -0300

    Updates

---
+++ precache-manifest.eece094571f9679fde73f07ae3c62f18.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "306ef22e052db6534da2f155b4441681",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "0a88f40cafaf075a582d",
+    "url": "/covid-19/static/css/main.f42784a1.chunk.css"
+  },
+  {
+    "revision": "f0625b42f6b1b511e90a",
+    "url": "/covid-19/static/js/2.d59b81a8.chunk.js"
+  },
+  {
+    "revision": "2c24acf63e18487b21e343b8335b2718",
+    "url": "/covid-19/static/js/2.d59b81a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a88f40cafaf075a582d",
+    "url": "/covid-19/static/js/main.2b8e8c31.chunk.js"
+  },
+  {
+    "revision": "c59e2f922549c307064d",
+    "url": "/covid-19/static/js/runtime-main.aabceb11.js"
+  },
+  {
+    "revision": "bb856027c5fc41db9aea492ea7470d2c",
+    "url": "/covid-19/static/media/logo.bb856027.png"
+  }
+]);
